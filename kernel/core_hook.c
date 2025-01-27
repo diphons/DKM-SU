0c8cd9b29e7457f0e7c57b634eabd06f1a6b808f
Wang Han <416810799@qq.com>
2025-01-27 19:35:33 +0800

    Clean up umount targets (#2386)

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -571,11 +571,11 @@ int ksu_handle_setuid(struct cred *new, const struct cred *old)
 	try_umount("/system_ext", true, 0);
 	try_umount("/vendor", true, 0);
 	try_umount("/product", true, 0);
+	try_umount("/system_ext", true, 0);
 	try_umount("/data/adb/modules", false, MNT_DETACH);
 
 	// try umount ksu temp path
 	try_umount("/debug_ramdisk", false, MNT_DETACH);
-	try_umount("/sbin", false, MNT_DETACH);
 	
 	// try umount hosts file
 	try_umount("/system/etc/hosts", false, MNT_DETACH);
